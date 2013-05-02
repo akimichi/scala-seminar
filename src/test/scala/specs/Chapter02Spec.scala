ce23b6c7bdb8f3a30dd3b8b68b21b91d8560d71b
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2013-05-02 22:26:11 +0900

    chap 2.7 まで

--- src/test/scala/specs/Chapter02Spec.scala
+++ src/test/scala/specs/Chapter02Spec.scala
@@ -67,7 +67,7 @@ class Chapter02Spec extends FunSpec with ShouldMatchers with helpers {
         breakable {
           for(i <- 1 to 100) {
             sum = sum + i
-            if(i > 5)
+            if(i >= 6)
               break
           }
         }
@@ -75,6 +75,7 @@ class Chapter02Spec extends FunSpec with ShouldMatchers with helpers {
       }
       it("再帰でbreak同等を実現する"){
         val upto = 6
+        @annotation.tailrec
         def loop(range:Range,accumulator:Int):Int = {
           val head = range.head
           if(head > upto)
@@ -84,8 +85,95 @@ class Chapter02Spec extends FunSpec with ShouldMatchers with helpers {
         }
         val range:Range = 1 to 100
         loop(range, 0) should equal(21)
+        info("この再帰の例は下記の高階関数と比べると冗長だが、引数で accumulator を利用するテクニックは「状態」を保持する方法として重要である")
+        info("上記の再帰関数は末尾再帰となっている点に注意")
+      }
+      it("高階関数で同様の機能を実現する"){
+        val range = 1 to 100
+        val upto = 6
+        range.take(upto).foldLeft(0){ (accum, item) =>
+          accum + item
+        } should equal(21)
       }
     }
+    describe("Sec 2.6"){
+      it("入れ子のループも1行で記述できる"){
+        for(i <- 1 to 3; from = 4 - i; j <- from to 3)
+          print((10 * i + j) + " ")
+        info("13 22 23 31 32 33 と出力される")
+      }
+      it("yieldでmapと同様の処理が可能である"){
+        val result = for(i <- 1 to 10) yield i % 3
+        result should equal(Seq(1, 2, 0, 1, 2, 0, 1, 2, 0, 1))
+      }
+    }
+    describe("Sec 2.7"){
+      import scala.math.BigInt
+      it("再帰関数の定義には、返値の指定が必須である"){
+        // @annotation.tailrec
+        def fac(n: BigInt): BigInt = if (n <= 0) 1 else n * fac(n - 1)
+        fac(10) should equal(3628800)
+        info("この再帰関数は末尾再帰になっていない点に注意")
+
+        def facrec(n: BigInt, accum:BigInt): BigInt = {
+          if (n <= 0)
+            accum
+          else
+            facrec(n - 1, n * accum)
+        }
+        facrec(10,1) should equal(3628800)
+      }
+      it("並列化する"){
+        object parallel {
+          import akka.dispatch.{Await, ExecutionContext, Future}
+          import akka.util.Timeout
+          import akka.util.duration._
+          import java.util.concurrent.{TimeoutException, Executors}
+          val executorService = Executors.newFixedThreadPool(8)
+          implicit val context = ExecutionContext.fromExecutor(executorService)
+          implicit val timeout = Timeout(5 seconds)
+          
+          def facrec(n: BigInt, downto:BigInt, accum:BigInt): BigInt = {
+            if (n == downto)
+              accum
+            else
+              facrec(n - 1, downto, n * accum)
+          }
+          def facrec_par():BigInt = {
+            val listOfFutures:List[Future[BigInt]] = List(Future{facrec(6000,5000,1)}.mapTo[BigInt],
+                                                          Future{facrec(5000,4000,1)}.mapTo[BigInt],
+                                                          Future{facrec(4000,3000,1)}.mapTo[BigInt],
+                                                          Future{facrec(3000,2000,1)}.mapTo[BigInt],
+                                                          Future{facrec(2000,1000,1)}.mapTo[BigInt],
+                                                          Future{facrec(1000,1,1)}.mapTo[BigInt])
+            val one:BigInt = 1
+            Await.result(Future.fold(listOfFutures)(one)(_ * _), timeout.duration).asInstanceOf[BigInt]
+          }
+        }
+        import parallel._
+        facrec_par() should equal(facrec(6000,0,1) )
+      }
+    }
+    describe("Sec 2.8"){
+      it("デフォルト引数の指定の仕方"){
+        def decorate(str: String, left: String = "[", right: String = "]") = left + str + right
+      }
+
+      
+    }
+    describe("Sec 2.9"){
+      
+    }
+    describe("Sec 2.10"){
+      
+    }
+    describe("Sec 2.11"){
+      
+    }
+    describe("Sec 2.12"){
+      
+    }
+
   }
 }
 
