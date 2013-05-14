567b96cec7e9ba1b81bb378f8f866e86284f94ef
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2013-05-14 23:57:09 +0900

    Chap3に加筆

--- src/test/scala/specs/Chapter03Spec.scala
+++ src/test/scala/specs/Chapter03Spec.scala
@@ -8,39 +8,48 @@ import scala_seminar._
 
 class Chapter03Spec extends FunSpec with ShouldMatchers with helpers {
   describe("Chap 3."){
+    describe("Arrayの問題点"){
+      it("Arrayはmutableである"){
+        val array = Array(1,2,3,4,5)
+        array(1) should equal(2)
+        array(1) = 0
+        array(1) should equal(0)
+      }
+    }
+    info("マルチスレッドで動作する場合は、immutable なListやVectorを使うべき")
     info("ランダムアクセスが必要な場合は、Listではなく、Vectorを使うべき")
-    info("マルチスレッドで動作する場合は、ArrayListではなく、immutable なVectorを使うべき")
     info("src/test/resources/scala_collection_hierachy.png")
-
     describe("ListとVectorの性能比較"){
       /*
-       *
        * val max = 10000000
        * val vector = Vector.range(1, max)
        * val list = List.range(1, max)
-       * time{list(max - 2)}
-       * time{vector(max - 2)}
+       * time{list.last}
+       * time{vector.last}
        * time{vector(0)}
        * time{list(0)}
        */ 
-
-      
     }
-    describe("パターン"){
-      trait Abstract {
-        val collection:Seq[Int]
-        def get(index:Int):Int = collection(index)
-      }
-
-      it("テスト"){
+    describe("使い分けのためのパターン"){
+      it("traitの場合"){
+        trait Abstract {
+          val collection:Seq[Int]
+        }
+        
         val list = new Abstract {
           val collection = List.range(1,100000)
         }
-        list.get(0)
+        list.collection(0) should equal(1)
         val vector  = new Abstract {
           val collection = Vector.range(1,100000)
         }
+        vector.collection(99998) should equal(99999)
       }
+      it("methodの場合"){
+        def get[T](collection:Seq[T], index:Int):T = collection(index)
+        get(List.range(1,100000),0) should equal(1)
+        get(Vector.range(1,100000),99998) should equal(99999)
+      }        
     }
   }
 }
