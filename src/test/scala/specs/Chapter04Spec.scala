c547f862bb6c7a21df7afb84b89a719520a06023
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2013-05-22 18:12:46 +0900

    add Chap 04

---
+++ src/test/scala/specs/Chapter04Spec.scala
@@ -0,0 +1,34 @@
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.FunSuite
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.{ FunSpec, BeforeAndAfterAll, BeforeAndAfterEach }
+
+import scala_seminar._
+
+class Chapter04Spec extends FunSpec with ShouldMatchers with helpers {
+  describe("Chap 4."){
+    describe("sec 4.1"){
+    }
+    describe("sec 4.2"){
+    }
+    describe("sec 4.3"){
+    }
+    describe("sec 4.4"){
+    }
+    describe("sec 4.5"){
+    }
+    describe("sec 4.6"){
+      it("javaと連携する"){
+      }
+    }
+    describe("sec 4.7"){
+      it("Tuple"){
+      }
+    }
+    describe("sec 4.8"){
+      it("zipする"){
+      }
+    }
+  }
+}
