f92f4b59bf5bbd22d498e5e2f50765d708afc72a
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2013-05-27 11:32:08 +0900

    update chap05

--- src/test/scala/specs/Chapter04Spec.scala
+++ src/test/scala/specs/Chapter04Spec.scala
@@ -182,57 +182,6 @@ class Chapter04Spec extends FunSpec with ShouldMatchers with helpers {
           }
         }
       }
-      describe("発展"){
-        type Name = String
-        type Iden = Int
-        type Mark = Int
-
-        type Codes = List[(Name,Iden)]
-        type Marks = List[(Iden,Mark)]
-
-        val codes:Codes = List("ANDERSON" -> 101372,
-                               "BAYLIS" -> 101369,
-                               "CARTER" -> 101370,
-                               "DENNIS" -> 101371,
-                               "EDWARD" -> 101373)
-        val marks:Marks = List(101369 -> 62,
-                               101370 -> 75,
-                               101371 -> 62,
-                               101372 -> 30,
-                               101373 -> 50)
-                          
-        def pair[A,B,C] : Pair[A => B, A => C] => A => Pair[B,C] = {(fg:Pair[A => B, A => C]) => { x:A =>
-          val (f,g) : Pair[A => B, A => C] = fg
-          (f(x), g(x))
-        }}
-        /* 
-         cross(xs ys) = [(x, y) |  x <- xs, y <- ys]
-         */ 
-        def cross[A,B,C,D] : Pair[A => B, C => D] => Pair[A,C] => Pair[B,D] = {(fg:Pair[A => B, C => D]) => { ac:Pair[A,C] =>
-          val (f,g) : Pair[A => B, C => D] = fg
-          val (a,c) : Pair[A,C] = ac
-          val (b,d) : Pair[B,D] = (f(a), g(c))
-          (b,d)
-        }}
-
-        /*
-        def collate(codes:Codes, marks:Marks) : List[Pair[Name,Mark]] = {
-          val ids:List[Iden] = for {
-            code <- codes
-            mark <- marks
-            
-          } yield {
-            val ab : (Name,Iden) => Name = {(name,iden) =>
-              name
-            }
-            val bd : (Iden, Mark) => Mark = {(iden,mark) =>
-              mark
-            }
-            cross[(Name,Iden),Name,(Iden,Mark),Mark]((ab,bd))(code, mark)
-          }
-        }
-        */ 
-      }
     }
     describe("sec 4.8"){
       it("zipする"){
---
+++ src/test/scala/specs/Chapter05Spec.scala
@@ -0,0 +1,12 @@
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.FunSuite
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.{ FunSpec, BeforeAndAfterAll, BeforeAndAfterEach }
+
+import scala_seminar._
+
+class Chapter05Spec extends FunSpec with ShouldMatchers with helpers {
+  describe("Chap 5."){
+  }
+}
