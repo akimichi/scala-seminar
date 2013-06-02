65a28f3efa5ddb9d3b6efb17618dad312efc8e49
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2013-06-03 01:14:48 +0900

    chap05を開始

--- src/test/scala/specs/Chapter05Spec.scala
+++ src/test/scala/specs/Chapter05Spec.scala
@@ -8,5 +8,19 @@ import scala_seminar._
 
 class Chapter05Spec extends FunSpec with ShouldMatchers with helpers {
   describe("Chap 5."){
+    describe("sec 5.1"){
+      it("クラスからインスタンスを作成する"){
+        class Counter {
+          private var value = 0 // You must initialize the field
+          def increment() { value += 1 } // Methods are public by default
+          def current() = value
+        }
+        val myCounter = new Counter
+        myCounter.increment()
+        myCounter.current should equal(0)
+
+      }
+    }
+
   }
 }
