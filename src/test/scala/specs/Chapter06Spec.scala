94b13aa5dc76872aca3f713cebf97884150805a5
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2013-06-14 00:53:42 +0900

    Chap 06 をほぼ作成

--- src/test/scala/specs/Chapter06Spec.scala
+++ src/test/scala/specs/Chapter06Spec.scala
@@ -19,17 +19,18 @@ class Chapter06Spec extends FunSpec with ShouldMatchers with helpers {
     }
     describe("sec 6.2"){
       class Account {
-        val id = Account.newUniqueNumber() // アクセス可能だが、スコープ内ではない。
+        val id = Account.newUniqueNumber() // class Account内からアクセス可能だが、スコープ内ではない。
         private var balance = 0.0
         def deposit(amount: Double) { balance += amount }
       }
       info("同一ソースファイル中に、同じ名前の class と object があるとき、その object を companion object という")
       object Account { // The companion object
+        def apply() = new Account
         private var lastNumber = 0
         def newUniqueNumber() = { lastNumber += 1; lastNumber }
       }
-      val account01 = new Account
-      val account02 = new Account
+      val account01 = Account()
+      val account02 = Account()
       account01.id should equal(1)
       account02.id should equal(2)
     }
@@ -50,7 +51,28 @@ class Chapter06Spec extends FunSpec with ShouldMatchers with helpers {
         override def redo() {once()}
       }
       val actions = Map("open" -> DoNothingAction, "save" -> DoNothingAction, "print" -> new PrintAction("test"))
-      
+      describe("MyOptionを定義する"){
+        sealed abstract class MyOption[+A] {
+          def isEmpty:Boolean
+          def get: A
+          def getOrElse[B >: A](default: => B): B = {
+            if (isEmpty) default else this.get
+          }
+        }
+        case class MySome[+A](a: A) extends MyOption[A] {
+          def isEmpty = false
+          def get = a
+        }
+        case object MyNone extends MyOption[Nothing] {
+          def isEmpty = true
+          def get = throw new NoSuchElementException("MyNone.get")
+        }
+        it("MyOptionを使う"){
+          val option = MySome(1)
+          option.getOrElse(0) should equal(1)
+        }
+      }
+      info("Chapter01Specの MyList における、MyNilもシングルトンオブジェクトである")
     }
     describe("sec 6.4"){
       object test {
@@ -60,11 +82,10 @@ class Chapter06Spec extends FunSpec with ShouldMatchers with helpers {
         object Account { // The companion object
           private var lastNumber = 0
           private def newUniqueNumber() = { lastNumber += 1; lastNumber }
-          def apply(initialBalance: Double) =
-            new Account(newUniqueNumber(), initialBalance)
+          def apply(initialBalance: Double) = new Account(newUniqueNumber(), initialBalance)
         }
       }
-      it("Account.apply"){
+      it("Account.applyで Accountインスタンスを生成する"){
         import test._
         val acct = Account(1000.0)
         acct.id  should equal(1)
@@ -73,6 +94,7 @@ class Chapter06Spec extends FunSpec with ShouldMatchers with helpers {
 
       it("Array(100) と new Array(100) は意味が異なる"){
         Array(100) should not equal(new Array(100))
+        Array(100)(0) should equal(100)
       }
     }
     describe("sec 6.5"){
@@ -88,9 +110,17 @@ class Chapter06Spec extends FunSpec with ShouldMatchers with helpers {
         object TrafficLightColor extends Enumeration {
           val Red, Yellow, Green = Value
         }
-        
+        import TrafficLightColor._
+        def doWhat(color: TrafficLightColor.Value):String = {
+          color match {
+            case Red => "stop"
+            case Yellow => "hurry up"
+            case _ => "go"
+          }
+        }
+        doWhat(Red) should equal("stop")
       }
-      it("type alias"){
+      it("type aliasを使う"){
         object TrafficLightColor extends Enumeration {
           type TrafficLightColor = Value
           val Red, Yellow, Green = Value
@@ -124,16 +154,20 @@ class Chapter06Spec extends FunSpec with ShouldMatchers with helpers {
       }
     }
     describe("補足"){
-      it("singletonのスコープ"){
+      it("objectの唯一性にはスコープがある"){
         trait Trait {
           object ObjectInTrait {
             var lastNumber = 0
-            def newUniqueNumber() = { lastNumber += 1; lastNumber }
+            def increment() = { lastNumber += 1}
+            
           }
         }
         val a = new Trait {}
-        println(a.ObjectInTrait)
         a.ObjectInTrait.lastNumber should equal(0)
+        a.ObjectInTrait.increment()
+        a.ObjectInTrait.lastNumber should equal(1)
+        val b = new Trait {}
+        b.ObjectInTrait.lastNumber should equal(0)
       }
     }
   }
