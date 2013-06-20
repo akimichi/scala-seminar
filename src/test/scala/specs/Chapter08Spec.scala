d571afc72848768da9d67c1f28e393cee264f607
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2013-06-20 17:53:03 +0900

    update chap08

--- src/test/scala/specs/Chapter08Spec.scala
+++ src/test/scala/specs/Chapter08Spec.scala
@@ -129,14 +129,97 @@ class Chapter08Spec extends FunSpec with ShouldMatchers with helpers {
       alien.greeting should equal("Greetings, Earthling! My name is Fred.")
     }
     describe("sec 8.8: Abstract Classes"){
-      
+      abstract class Person(val name: String) {
+        def id: Int // 抽象メソッド
+      }
+      class Employee(name: String) extends Person(name) {
+        def id = name.hashCode // override keyword not required
+      }
+      val adam = new Employee("adam")
+      adam.id should equal(2988943)
     }
     describe("sec 8.9: Abstract Fields"){
-      
+      abstract class Person {
+        val id: Int // No initializer—this is an abstract field with an abstract getter method
+        var name: String // Another abstract field, with abstract getter and setter methods
+      }
+      class Employee(val id: Int) extends Person { // Subclass has concrete id property
+        var name = "undefined" // and concrete name property
+      }
+      val adam = new Employee(1)
+      adam.name should equal("undefined")
+      adam.name = "adam"
+      adam.name should equal("adam")
+      it("無名クラスでインスタンス化する"){
+        val fred = new Person {
+          val id = 1729
+          var name = "Fred"
+        }
+        fred.name should equal("Fred")
+      }
     }
     describe("sec 8.10: Construction Order and Early Definitions"){
+      class Creature {
+        val range: Int = 10
+        val env: Array[Int] = new Array[Int](range) // ここで range を使っているのが問題となる
+      }
+      it("コンストラクタ初期化の順序がまぎらわしい例"){
+        class Ant extends Creature {
+          override val range = 2
+        }
+//      intercept[scala.UninitializedFieldError]{
+        val ant = new Ant
+        ant.range should equal(2)
+        ant.env.length should equal(0)
+//      }
+      }
+      it("early definitionによる解決"){
+        class Bug extends {
+          override val range = 3
+        } with Creature
+        val bug = new Bug
+        bug.range should equal(3)
+        bug.env.length should equal(3)
+      }
+      it("補足: lazy valによる解決"){
+        class Creature {
+          def range: Int = 10
+          val env: Array[Int] = new Array[Int](range) // ここで range を使っているのが問題となる
+        }
+        class Worm extends Creature {
+          override lazy val range = 5
+        } 
+        val worm = new Worm
+        worm.range should equal(5)
+        worm.env.length should equal(5)
+      }
+    }
+    describe("sec 8.11: The Scala Inheritance Hierarchy"){
       
     }
-
+    describe("sec 8.12: Object Equality"){
+      it("等値演算"){
+        val a_string = new String("string")
+        val another_string = new String("string")
+        (a_string.eq(another_string)) should equal(false)
+        (a_string.eq(a_string)) should equal(true)
+        
+      }
+      it("equalsを再定義する"){
+        class Item(val name: String, val price: Double, val color:String){
+          final override def equals(other: Any) = {
+            val that = other.asInstanceOf[Item]
+            if (that == null) false
+            else name == that.name && price == that.price
+          }
+          final override def hashCode = 13 * name.hashCode + 17 * price.hashCode
+        }
+        val an_item = new Item("an item",10.0, "Red")
+        val another_item = new Item("an item",10.0, "Black")
+        an_item.equals(another_item) should equal(true)
+      }
+    }
+    describe("補足:case class では、equalsとhashCodeがメンバーから自動的に定義されている"){
+    }
   }
 }
