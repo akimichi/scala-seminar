c7d494bc8d3a183ea2fb3c407191a8246cc636c1
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2013-06-27 01:04:51 +0900

    working on Chap10

--- src/test/scala/specs/Chapter10Spec.scala
+++ src/test/scala/specs/Chapter10Spec.scala
@@ -8,11 +8,12 @@ import org.scalatest.{ FunSpec, BeforeAndAfterAll, BeforeAndAfterEach }
 import scala_seminar._
 
 class Chapter10Spec extends FunSpec with ShouldMatchers with helpers {
-  trait Action[T] {
-    val arg:T
+  trait Action[+T] {
+    def arg:T
     def invoke:Unit
   }
   trait EmptyAction[T] extends Action[T] {
+    def arg:Nothing = throw new Exception("should not be called")
     def invoke:Unit = { }
   }
   trait ConsoleAction[T] extends Action[T] {
@@ -25,20 +26,29 @@ class Chapter10Spec extends FunSpec with ShouldMatchers with helpers {
   }
   describe("sec 10.2: Traits as Interfaces"){
     trait Logger {
-      def log(msg: String) // An abstract method
+      def log(msg: String):Action[String] // An abstract method
     }
     
     class ConsoleLogger extends Logger { // Use extends, not implements
-      def log(msg: String) { println(msg) } // No override needed
+      def log(msg: String):ConsoleAction[String] = { // No override needed
+       new ConsoleAction[String] { val arg = msg}
+      }
     }
     class SerializableLogger extends Logger with Serializable {
-      def log(msg: String) { println(msg) } // No override needed
+      def log(msg: String):ConsoleAction[String] = { // No override needed
+       new ConsoleAction[String] { val arg = msg }
+      }
     }
-    
+    it("ConsoleLoggerを使う"){
+      val logger = new ConsoleLogger
+      logger.log("this is a test") should be (anInstanceOf[ConsoleAction[String]])
+    }
   }
   describe("sec 10.3: Traits with Concrete Implementations"){
     trait ConsoleLogger {
-      def log(msg: String) = msg
+      def log(msg: String):ConsoleAction[String] = new ConsoleAction[String] {
+        val arg = msg
+      }
     }
     trait Account {
       var balance:Double
@@ -46,22 +56,26 @@ class Chapter10Spec extends FunSpec with ShouldMatchers with helpers {
     class SavingsAccount(val _balance:Double) extends Account with ConsoleLogger {
       override var balance:Double = _balance
       
-      def withdraw(amount: Double) {
+      def withdraw(amount: Double):Action[String] = {
         if (amount > balance)
           log("Insufficient funds")
         else {
           balance -= amount
+          new EmptyAction[Nothing] {}
         }
       }
     }
-
+    it("SavingsAccountを使う"){
+      val account = new SavingsAccount(100)
+      account.withdraw(10)
+      account.balance should equal(90)
+      account.withdraw(100) should be (anInstanceOf[ConsoleAction[String]])
+    }
   }
   describe("sec 10.4: Objects with Traits"){
-    
+    info("インスタンスに trait を mixin 可能である")
     trait Logged {
-      def log(msg: String):Action[String] =  new EmptyAction[String] {
-        val arg = msg
-      }
+      def log(msg: String):Action[String] =  new EmptyAction[String] {}
     }
     trait Account {
       var balance:Double = 0.0
@@ -75,16 +89,10 @@ class Chapter10Spec extends FunSpec with ShouldMatchers with helpers {
       }
     }
     trait ConsoleLogger extends Logged {
-      override def log(msg: String):Action[String] = {
-        new ConsoleAction[String] {
-          val arg = msg
-        }
-      }
+      override def log(msg: String):Action[String] = new ConsoleAction[String] { val arg = msg }
     }
     trait FileLogger extends Logged {
-      override def log(msg: String) = new FileAction[String] {
-        val arg = msg
-      }
+      override def log(msg: String) = new FileAction[String] { val arg = msg }
     }
     val acct = new SavingsAccount with ConsoleLogger
     val acct2 = new SavingsAccount with FileLogger
@@ -93,26 +101,192 @@ class Chapter10Spec extends FunSpec with ShouldMatchers with helpers {
   describe("sec 10.5: Layered Traits"){
 
     trait Logged {
-      def log(msg: String):Action[String] =  new EmptyAction[String] {
+      def log(msg: String):Action[String] = new ConsoleAction[String] { val arg = msg }
+    }
+
+    trait ConsoleLogger extends Logged {
+      override def log(msg: String):Action[String] = new ConsoleAction[String] { val arg = msg }
+    }
+    trait TimestampLogger extends Logged {
+      import java.text._
+      override def log(msg: String):Action[String] = {
+        val date = {
+          val df:DateFormat = new SimpleDateFormat("yyyy/MM/dd")
+          df.parse("2013/06/26")
+        }
+        super.log(date + " " + msg)
+      }
+    }
+    trait ShortLogger extends Logged {
+      val maxLength = 15
+      override def log(msg:String):Action[String] = {
+        super.log(
+          if (msg.length <= maxLength) msg else msg.substring(0,maxLength -3) + "..."
+        )
+      }
+    }
+    trait Account {
+      var balance:Double
+    }
+    class SavingsAccount(val _balance:Double) extends Account with Logged {
+      override var balance:Double = _balance
+      def withdraw(amount: Double):Action[String] = {
+        if (amount > balance)
+          log("Insufficient funds")
+        else {
+          balance -= amount
+          log("The balance is now %f".format(balance))
+        }
+      }
+    }
+    
+    val acct1 = new SavingsAccount(100) with ConsoleLogger with TimestampLogger with ShortLogger
+    val acct2 = new SavingsAccount(100) with ConsoleLogger with ShortLogger with TimestampLogger
+    acct1.withdraw(10).arg should equal("Wed Jun 26 00:00:00 JST 2013 The balance ...")
+    acct2.withdraw(10).arg should equal("Wed Jun 26 0...")
+  }
+  describe("sec 10.6: Overriding Abstract Methods in Traits"){
+    trait Logger {
+      def log(msg:String):Action[String]
+    }
+    trait TimestampLogger extends Logger {
+      import java.text._
+      abstract override def log(msg: String):Action[String] = {
+        val date = {
+          val df:DateFormat = new SimpleDateFormat("yyyy/MM/dd")
+          df.parse("2013/06/26")
+        }
+        super.log(date + " " + msg)
+      }
+    }
+  }
+  describe("sec 10.7: Traits for Rich Interfaces"){
+    trait Logger {
+      def log(msg:String):Action[String]
+      def info(msg:String):Action[String] = { log("INFO: " + msg) }
+      def warn(msg:String):Action[String] = { log("WARN: " + msg) }
+      def severe(msg:String):Action[String] = { log("SEVERE: " + msg) }
+    }
+    class SavingsAccount(var balance:Double) extends Logger {
+      def log(msg:String):Action[String] = new ConsoleAction[String] { val arg = msg }
+      def withdraw(amount: Double):Action[String] = {
+        if (amount > balance)
+          severe("Insufficient funds")
+        else {
+          balance -= amount
+          info("The balance is now %f".format(balance))
+        }
+      }
+    }
+    val acct1 = new SavingsAccount(100)
+    acct1.withdraw(10).arg should equal("INFO: The balance is now 90.000000")
+    acct1.withdraw(1000).arg should equal("SEVERE: Insufficient funds")
+  }
+  describe("sec 10.8: Concrete Fields in Traits"){
+    it("trait が具象メンバーを持つ場合、その具象メンバーはサブクラスに継承されるのではなく、追加される"){
+      trait Logged {
+        def log(msg: String):Action[String] = new ConsoleAction[String] { val arg = msg }
+      }
+      trait ShortLogger extends Logged {
+        val maxLength = 15
+        override def log(msg:String):Action[String] = {
+          super.log(
+            if (msg.length <= maxLength) msg else msg.substring(0,maxLength -3) + "..."
+          )
+        }
+      }
+      class Account {
+        var balance:Double = 0.0
+      }
+      class SavingsAccount extends Account with ShortLogger {
+        def withdraw(amount: Double):Action[String] = {
+          if (amount > balance)
+            log("Insufficient funds")
+          else {
+            balance -= amount
+            log("The balance is now %f".format(balance))
+          }
+        }
+      }
+      
+      
+    }
+  }
+  describe("sec 10.9: Abstract Fields in Traits"){
+    it("抽象メンバーを持つtraitをmixinする"){
+      trait Logged {
+        def log(msg: String):Action[String] = new ConsoleAction[String] { val arg = msg }
+      }
+      trait ShortLogger extends Logged {
+        val maxLength:Int
+        override def log(msg:String):Action[String] = {
+          super.log(
+            if (msg.length <= maxLength) msg else msg.substring(0,maxLength -3) + "..."
+          )
+        }
+      }
+      class SavingsAccount(var balance:Double) extends ShortLogger {
+        val maxLength = 20 // 抽象メンバーを具体化する必要がある
+        def withdraw(amount: Double):Action[String] = {
+          if (amount > balance)
+            log("Insufficient funds")
+          else {
+            balance -= amount
+            log("The balance is now %f".format(balance))
+          }
+        }
+      }
+    }
+  }
+  describe("sec 10.10: Trait Construction Order"){
+    trait Logged {
+      println("Logged construction")
+      def log(msg: String):Action[String] = new ConsoleAction[String] { val arg = msg }
+    }
+    trait FileLogger extends Logged {
+      println("FileLogger construction")
+      import java.text._
+      import java.io.PrintWriter
+      val out = new PrintWriter("app.log")
+      val date = {
+        val df:DateFormat = new SimpleDateFormat("yyyy/MM/dd")
+        df.parse("2013/06/26")
+      }
+      out.println("# " + date.toString)
+      override def log(msg: String) = new FileAction[String] {
+        out.println(msg)
+        out.flush
         val arg = msg
       }
     }
-
-    trait TimestampLogger extends Logged {
-      override def log(msg: String):Action[String] = {
-        super.log(new java.util.Date() + " " + msg)
+    trait ShortLogger extends Logged {
+      println("ShortLogger construction")
+      val maxLength = 15
+      override def log(msg:String):Action[String] = {
+        super.log(
+          if (msg.length <= maxLength) msg else msg.substring(0,maxLength -3) + "..."
+        )
       }
     }
-  }
-  describe("sec 10.6: Overriding Abstract Methods in Traits"){
-  }
-  describe("sec 10.7: Traits for Rich Interfaces"){
-  }
-  describe("sec 10.8: Concrete Fields in Traits"){
-  }
-  describe("sec 10.9: Abstract Fields in Traits"){
-  }
-  describe("sec 10.10: Trait Construction Order"){
+    trait Account {
+      println("Account construction")
+      var balance:Double
+    }
+    class SavingsAccount(val _balance:Double) extends Account with ShortLogger {
+      override var balance = _balance
+      println("SavingsAccount construction")
+      def withdraw(amount: Double):Action[String] = {
+        if (amount > balance)
+          log("Insufficient funds")
+        else {
+          balance -= amount
+          log("The balance is now %f".format(balance))
+        }
+      }
+    }
+    val acct1 = new SavingsAccount(100)
+    acct1.withdraw(10).arg should equal("The balance ...")
+    info("Account,Logged, ShortLogger, SavingsAccount の順序で初期化されている")
   }
   describe("sec 10.11: Initializing Trait Fields"){
   }
