e88a288badf3a34174f63a5a64ccb6e95980ad6d
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2013-08-01 16:20:15 +0900

    created Chapter13Spec

---
+++ src/test/scala/specs/Chapter13Spec.scala
@@ -0,0 +1,12 @@
+package test
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.FunSuite
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.{ FunSpec, BeforeAndAfterAll, BeforeAndAfterEach }
+
+import scala_seminar._
+
+class Chapter13Spec extends FunSpec with ShouldMatchers with helpers {
+}
+    
