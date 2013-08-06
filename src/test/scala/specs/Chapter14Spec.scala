a91d8762ed65cf5749c6f76a27a1e2bb59dc6d5e
Akimichi Tatsukawa <akimichi.tatsukawa@gmail.com>
2013-08-06 20:24:57 +0900

    add Chapter14Spec

---
+++ src/test/scala/specs/Chapter14Spec.scala
@@ -0,0 +1,10 @@
+package test
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.FunSuite
+import org.scalatest.matchers.ShouldMatchers
+import org.scalatest.FunSpec
+import scala_seminar._
+
+class Chapter14Spec extends FunSpec with ShouldMatchers with helpers {
+}
